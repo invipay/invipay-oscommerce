1336e0ad08fd31797b2820205caabcd519fd6036
Kuba Pilecki <k.pilecki@gmail.com>
2016-02-09 21:47:59 +0100

    no message

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # invipay-oscommerce
 inviPay OS Commerce Paygate Plugin
+
+2.3 ISO-8859-2 version
---
+++ includes/languages/polish/modules/payment/invipay.php
@@ -0,0 +1,14 @@
+<?php
+  define('MODULE_PAYMENT_INVIPAY_TEXT_TITLE', '<img src="http://www.invipay.com/promo/images/logo_slogan_small.png" height="20px" align="middle" alt="Zakupy na fakturę z '.MODULE_PAYMENT_INVIPAY_PAYDATE.'-dniowym terminem płatno¶ci (tylko dla firm)" />Zakupy na fakturę z '.MODULE_PAYMENT_INVIPAY_PAYDATE.'-dniowym terminem płatno¶ci (tylko dla firm)');
+  define('MODULE_PAYMENT_INVIPAY_TEXT_DESCRIPTION', 'Po odbiorze zamówionego towaru, zapłacisz za niego w terminie płatno¶ci wynikaj±cym z faktury ('.MODULE_PAYMENT_INVIPAY_PAYDATE.' dni), któr± od nas otrzymasz za po¶rednictwem aplikacji inviPay.com powiększonym o 7 dni od inviPay.com (ł±cznie masz '.MODULE_PAYMENT_INVIPAY_PAYDATE.'+7 dni na zapłatę).');
+  
+  define('MODULE_PAYMENT_INVIPAY_STARTED_ORDER_STATUS_NAME', 'InviPay.com - płatno¶ć rozpoczęta');
+  define('MODULE_PAYMENT_INVIPAY_OUT_OF_LIMIT_ORDER_STATUS_NAME', 'InviPay.com - płatno¶ć anulowana');
+  define('MODULE_PAYMENT_INVIPAY_TIMEDOUT_ORDER_STATUS_NAME', 'InviPay.com - płatno¶ć porzucona');
+  define('MODULE_PAYMENT_INVIPAY_COMPLETED_ORDER_STATUS_NAME', 'InviPay.com - płatno¶ć zatwierdzona');
+  define('MODULE_PAYMENT_INVIPAY_DELIVERED_ORDER_STATUS_NAME', 'InviPay.com - przesyłka doręczona');
+  define('MODULE_PAYMENT_INVIPAY_FINISHED_ORDER_STATUS_NAME', 'InviPay.com - faktura przesłana');
+  
+  define('MODULE_PAYMENT_INVIPAY_MINCART_ERROR', 'Zakupy na fakturę z '.MODULE_PAYMENT_INVIPAY_PAYDATE.'-dniowym terminem płatno¶ci (tylko dla firm) dostępne s± dla zakupów o minimalnej warto¶ci '.MODULE_PAYMENT_INVIPAY_MINCART.' złotych');
+  define('MODULE_PAYMENT_INVIPAY_MINCART_INFO', 'Metoda płatno¶ci dostępna od '.MODULE_PAYMENT_INVIPAY_MINCART.' złotych, tylko dla firm');
+?>
